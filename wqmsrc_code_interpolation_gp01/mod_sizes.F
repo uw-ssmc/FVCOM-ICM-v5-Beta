f98b01c5be0ecff09ce1dbe9d88a8464f2a42c0b
Male Rajage L Premathilake <prem118@deception01.local>
2025-12-03 10:47:07 -0800

    Laki - added zooplankton DVM. Need to test and calibrate.

--- wqmsrc_code_interpolation_gp01/mod_sizes.F
+++
@@ -1,9 +0,0 @@
-!
-Module MOD_SIZES
-  !***** Parameter declarations
-  !***** ST. JOHNS RIVER COARSE GRID:6 LAYER MODEL
-      Integer, Parameter :: NCP = 34, NQFP = 32686, NHQP = 22086, NS1P &
-     & = 378, NS2P = 378, NS3P = 378, NBCP = 492, NDP = 500, NFLP = 20, &
-     & NOIP = 10, NSSFP = 10
-      Integer :: MGL, NGL, OBCGL, NOBTY
-End Module MOD_SIZES
